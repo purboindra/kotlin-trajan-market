018d0e14536fa4fccbb9763a11381a7a68cd4a37
Purbo Indra Gumelar <hellopurbo123>
2024-11-13 16:28:03 +0700

    fix: change background app color

--- app/src/main/java/com/example/trajanmarket/MainActivity.kt
+++ app/src/main/java/com/example/trajanmarket/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.trajanmarket.ui.navigation.AppNavHost
 import com.example.trajanmarket.ui.screens.login.LoginViewModel
@@ -15,14 +19,19 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        
+
         val loginViewModelL: LoginViewModel by viewModels()
-        
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TrajanMarketTheme {
-                MainAppContent()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainAppContent()
+                }
             }
         }
     }
--- app/src/main/java/com/example/trajanmarket/ui/theme/Theme.kt
+++ app/src/main/java/com/example/trajanmarket/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -19,9 +20,10 @@ private val DarkColorScheme = darkColorScheme(
 
 private val LightColorScheme = lightColorScheme(
     primary = yellow80,
+    background = Color(0xFFFFFBFE),
 
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
+
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
