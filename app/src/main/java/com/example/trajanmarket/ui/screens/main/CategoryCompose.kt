78be2e363aad046f1b975e8dfd1f0075daae0062
purboyndra <purboindra@gmail.com>
2024-11-14 06:45:46 +0700

    feat: CategoryCompose

---
+++ app/src/main/java/com/example/trajanmarket/ui/screens/main/CategoryCompose.kt
@@ -0,0 +1,17 @@
+package com.example.trajanmarket.ui.screens.main
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CategoryCompose() {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Box(contentAlignment = Alignment.Center){
+            Text("Browse by Category")
+        }
+    }
+}
