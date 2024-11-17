36b9a23a7583a407381e43f60c8cefcf5c62e9bb
purboyndra <purboindra@gmail.com>
2024-11-17 16:37:38 +0700

    refactor: remove launchedeffect RecommendedCompose

--- app/src/main/java/com/example/trajanmarket/ui/screens/home/RecommendedCompose.kt
+++ app/src/main/java/com/example/trajanmarket/ui/screens/home/RecommendedCompose.kt
@@ -1,6 +1,5 @@
 package com.example.trajanmarket.ui.screens.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,37 +7,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
+import androidx.navigation.NavHostController
 import com.example.trajanmarket.data.model.State
 import com.example.trajanmarket.ui.components.LoadingCard
 import com.example.trajanmarket.ui.components.ProductCategoryCardCompose
 import com.example.trajanmarket.utils.VerticalSpacer
 
 @Composable
-fun RecommendedCompose(homeViewModel: HomeViewModel) {
+fun RecommendedCompose(homeViewModel: HomeViewModel, navHostController: NavHostController) {
     
     val productState by homeViewModel.products.collectAsState()
-    
-    LaunchedEffect(Unit) {
-        homeViewModel.fetchAllProducts(
-            sortBy = "order", order = "asc", limit = "10"
-        )
-    }
     
     Box(
         modifier = Modifier
@@ -80,7 +70,7 @@ fun RecommendedCompose(homeViewModel: HomeViewModel) {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
-                            .height(((product.products.size-1) * 120.dp))
+                            .height(((product.products.size - 1) * 120.dp))
                     ) {
                         LazyVerticalGrid(
                             modifier = Modifier
@@ -92,14 +82,20 @@ fun RecommendedCompose(homeViewModel: HomeViewModel) {
                             userScrollEnabled = false,
                         ) {
                             items(product.products) { product ->
-                                ProductCategoryCardCompose(product, showPrice = true)
+                                ProductCategoryCardCompose(
+                                    product,
+                                    showPrice = true,
+                                    navHostController = navHostController
+                                )
                             }
                         }
                     }
                 }
+                
                 is State.Failure -> {
                     val throwable = (productState as State.Failure)
                 }
+                
                 else -> {}
             }
         }
