d60efc03bf33f73d4bf5e6db6bb96ec53f57f572
purboyndra <purboindra@gmail.com>
2024-11-17 12:41:01 +0700

    refactor: remove unnecessary box homescreen

--- app/src/main/java/com/example/trajanmarket/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/trajanmarket/ui/screens/home/HomeScreen.kt
@@ -1,19 +1,19 @@
 package com.example.trajanmarket.ui.screens.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.trajanmarket.utils.VerticalSpacer
 
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel()) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            item {
-                CategoryCompose(homeViewModel)
-            }
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            CategoryCompose(homeViewModel)
+            20.VerticalSpacer()
+            RecommendedCompose(homeViewModel)
         }
     }
 }
