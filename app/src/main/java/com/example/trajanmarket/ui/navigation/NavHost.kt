af08bb959972b01e598c839acca4cae90e13a603
purboyndra <purboindra@gmail.com>
2024-11-18 05:24:07 +0700

    fix: navhost route with argument product detail

--- app/src/main/java/com/example/trajanmarket/ui/navigation/NavHost.kt
+++ app/src/main/java/com/example/trajanmarket/ui/navigation/NavHost.kt
@@ -3,12 +3,15 @@ package com.example.trajanmarket.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.createGraph
+import androidx.navigation.navArgument
 import com.example.trajanmarket.ui.screens.login.LoginScreen
 import com.example.trajanmarket.ui.screens.main.MainScreen
+import com.example.trajanmarket.ui.screens.product.detail.ProductDetailScreen
 import com.example.trajanmarket.ui.screens.splash.SplashScreen
 
 @Composable
@@ -26,8 +29,14 @@ fun AppNavHost(navController: NavHostController = rememberNavController()) {
             composable<Main> {
                 MainScreen(navHostController = navController)
             }
-            composable<ProductDetail> { backStackEntry ->
-                ProductDetail(backStackEntry.id)
+            composable(
+                route = "product_detail/{productId}",
+                arguments = listOf(navArgument("productId") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val productId = backStackEntry.arguments?.getString("productId")
+                productId?.let {
+                    ProductDetailScreen(id = it)
+                }
             }
         }
     }
