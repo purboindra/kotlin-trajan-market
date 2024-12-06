5949594e5435e76fcefc315720795d6484e4bc77
purboyndra <purboindra@gmail.com>
2024-12-07 05:33:31 +0700

    feat: user id key dataStore

--- app/src/main/java/com/example/trajanmarket/data/local/datastore/DataStore.kt
+++ app/src/main/java/com/example/trajanmarket/data/local/datastore/DataStore.kt
@@ -7,6 +7,7 @@ import androidx.datastore.preferences.preferencesDataStore
 
 object PreferencesKey {
     val USER_NAME = stringPreferencesKey("user_name")
+    val USER_ID = stringPreferencesKey("user_id")
     val IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
     val USER_IMAGE = stringPreferencesKey("user_image")
     val ACCESS_TOKEN = stringPreferencesKey("access_token")
