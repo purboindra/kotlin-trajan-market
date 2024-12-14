a1a3cc65bd846142f1f34e6052d06215ba69a22a
purboyndra <purboindra@gmail.com>
2024-12-15 05:01:07 +0700

    feat: getCarts CartUseCase

--- app/src/main/java/com/example/trajanmarket/domain/usecases/CartUseCase.kt
+++ app/src/main/java/com/example/trajanmarket/domain/usecases/CartUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.trajanmarket.domain.usecases
 
+import com.example.trajanmarket.data.model.CartEntity
 import com.example.trajanmarket.data.model.State
 import com.example.trajanmarket.data.remote.api.AddToCartParams
 import com.example.trajanmarket.data.repository.CartRepository
@@ -9,4 +10,6 @@ import javax.inject.Inject
 class CartUseCase @Inject constructor(private val cartRepository: CartRepository) {
     fun addToCart(products: List<AddToCartParams>): Flow<State<Boolean>> =
         cartRepository.addToCart(products)
+    
+    fun getCarts(): Flow<State<List<CartEntity>>> = cartRepository.getCarts()
 }
