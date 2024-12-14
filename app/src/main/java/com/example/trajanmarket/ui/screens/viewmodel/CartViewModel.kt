1ab3fd4db925c65dc5ce5dc9024abb9bed06af70
purboyndra <purboindra@gmail.com>
2024-12-15 05:00:34 +0700

    feat: CartViewModel

---
+++ app/src/main/java/com/example/trajanmarket/ui/screens/viewmodel/CartViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.trajanmarket.ui.screens.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.trajanmarket.data.model.CartEntity
+import com.example.trajanmarket.data.model.State
+import com.example.trajanmarket.data.remote.api.AddToCartParams
+import com.example.trajanmarket.domain.usecases.CartUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CartViewModel @Inject constructor(private val cartUseCase: CartUseCase) : ViewModel() {
+    private val _addToCartState = MutableStateFlow<State<Boolean>>(State.Idle)
+    val addToCartState: StateFlow<State<Boolean>> = _addToCartState
+    
+    private val _cartListState =
+        MutableStateFlow<State<List<CartEntity>>>(State.Succes(emptyList()))
+    val cartListState: StateFlow<State<List<CartEntity>>> = _cartListState
+    
+    fun addToCart(products: List<AddToCartParams>) = viewModelScope.launch {
+        cartUseCase.addToCart(products).collectLatest { state ->
+            _addToCartState.value = state
+        }
+    }
+    
+    fun getCarts() = viewModelScope.launch {
+        cartUseCase.getCarts().collectLatest { state ->
+            _cartListState.value = state
+        }
+    }
+    
+}
