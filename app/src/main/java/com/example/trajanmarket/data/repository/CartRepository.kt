22084ca4d2308ceab1f419333ccb252464cb27d5
purboyndra <purboindra@gmail.com>
2024-12-15 05:00:54 +0700

    feat: addToCart cart dao CartRepository

--- app/src/main/java/com/example/trajanmarket/data/repository/CartRepository.kt
+++ app/src/main/java/com/example/trajanmarket/data/repository/CartRepository.kt
@@ -1,7 +1,9 @@
 package com.example.trajanmarket.data.repository
 
 import android.util.Log
+import com.example.trajanmarket.data.local.CartDao
 import com.example.trajanmarket.data.local.datastore.UserPreferences
+import com.example.trajanmarket.data.model.CartEntity
 import com.example.trajanmarket.data.model.State
 import com.example.trajanmarket.data.remote.api.AddToCartParams
 import com.example.trajanmarket.data.remote.api.CartApi
@@ -13,7 +15,11 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.serialization.json.Json
 
 
-class CartRepository(private val cartApi: CartApi, private val userPreferences: UserPreferences) {
+class CartRepository(
+    private val cartApi: CartApi,
+    private val userPreferences: UserPreferences,
+    private val cartDao: CartDao
+) {
     
     fun addToCart(products: List<AddToCartParams>): Flow<State<Boolean>> = flow {
         
@@ -23,9 +29,16 @@ class CartRepository(private val cartApi: CartApi, private val userPreferences: 
         try {
             val userId = userPreferences.userId.first() ?: throw Exception("User not valid")
             
-            val response = cartApi.addToCart(products,  userId)
+            val response = cartApi.addToCart(products, userId)
+            
+            val cartEntity = CartEntity(
+                productId = products[0].id.toInt(),
+                userId = userId.toInt(),
+                quantity = 1
+            )
             
             if (response.status.isSuccess()) {
+                cartDao.insert(cartEntity)
                 emit(State.Succes(true))
             } else {
                 val errorText = response.bodyAsText()
@@ -38,6 +51,21 @@ class CartRepository(private val cartApi: CartApi, private val userPreferences: 
             }
         } catch (e: Throwable) {
             Log.d("error add to cart", e.message ?: "Unknown error")
+            emit(State.Failure(e))
+        }
+    }
+    
+    fun getCarts(): Flow<State<List<CartEntity>>> = flow {
+        Log.d("getCarts", "Get carts cartRepository called")
+        
+        try {
+            val userId = userPreferences.userId.first()
+            userId?.let {
+                val result = cartDao.getCartByUserId(it.toInt())
+                emit(State.Succes(result))
+            }
+        } catch (e: Throwable) {
+            Log.d("error getCartByUserId", e.message ?: "Unknown error")
             emit(State.Failure(e))
         }
     }
