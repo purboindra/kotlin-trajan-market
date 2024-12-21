1cd68421544e06a6f01a05ddc7934f174dd1296b
purboyndra <purboindra@gmail.com>
2024-12-21 14:10:07 +0700

    feat: SearchViewModel

---
+++ app/src/main/java/com/example/trajanmarket/ui/screens/search/SearchViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.trajanmarket.ui.screens.search
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class SearchViewModel @Inject constructor() : ViewModel() {
+
+}
