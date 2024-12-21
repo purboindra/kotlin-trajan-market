64da003f7aa6118ee1364d3f63f4b27b7e583f8b
purboyndra <purboindra@gmail.com>
2024-12-21 19:13:16 +0700

    feat: searchProduct SearchViewModel.kt

--- app/src/main/java/com/example/trajanmarket/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/example/trajanmarket/ui/screens/search/SearchViewModel.kt
@@ -1,8 +1,54 @@
 package com.example.trajanmarket.ui.screens.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.trajanmarket.data.model.Product
+import com.example.trajanmarket.data.model.State
+import com.example.trajanmarket.domain.usecases.GetProductUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class SearchViewModel @Inject constructor() : ViewModel() {
-
+@HiltViewModel()
+class SearchViewModel @Inject constructor(private val productUseCase: GetProductUseCase) :
+    ViewModel() {
+    private val _searchState = MutableStateFlow<State<Product>>(State.Idle)
+    val searchState: StateFlow<State<Product>> = _searchState
+    
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery
+    
+    fun onQueryChanged(query: String) {
+        _searchQuery.value = query
+    }
+    
+    suspend fun resetState() {
+        _searchState.emit(State.Idle)
+    }
+    
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    fun observeSearchQuery() {
+        viewModelScope.launch {
+            _searchQuery.debounce(300).filter {
+                it.isNotEmpty()
+            }.distinctUntilChanged().flatMapLatest { query ->
+                productUseCase.searchProduct(query)
+            }.collectLatest { state ->
+                _searchState.value = state
+            }
+        }
+    }
+    
+    init {
+        observeSearchQuery()
+    }
 }
